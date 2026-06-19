e91b8e35ff99c79a993dfa618db0283208c29707
Mateus Joao Feldhaus <mjf@tutamail.com>
2026-06-19 13:38:15 -0300

    chore: remove pasta src e public duplicadas da raiz

--- src/pages/404NotFound/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const NotFound = () => {
-  return <div>Error 404 - Page Not Found</div>;
-};
--- src/styles/theme.ts
+++
@@ -1,5 +0,0 @@
-export const theme = {
-  colors: {
-    primary: "rgb(255, 208, 110)",
-  },
-};
